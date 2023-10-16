90ce302dfd3a1fac33b2e92f49ebab1d8b47a2d9
Jörn Heinemann <joernheinemann@gmx.de>
2023-10-16 23:24:53 +0200

    use feat

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(incomplete_features)]
+#![feature(async_fn_in_trait, type_alias_impl_trait, return_position_impl_trait_in_trait)]
+//
 #![doc = include_str!("../README.md")]
 #![allow(clippy::tabs_in_doc_comments)]
 //
